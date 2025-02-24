ceff877ff368bc1ce7556e13477d68bd56181aec
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2025-02-24 16:11:39 +0530

    change port

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,18 +1,19 @@
 require('dotenv').config({ path: '.env' });
-const cors=require('cors');
-
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+
 const doctorRoutes = require('./routes/doctorRoutes');
 const appointmentRoutes = require('./routes/AppointmentRoutes');
 const errorHandler = require('./middlewares/errorHandler');
-const port = process.env.PORT || 4000;
 
 const app = express();
+const port = process.env.PORT || 4000;  
+
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173',"*"],
+  origin: ['http://localhost:5173', '*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
@@ -20,10 +21,13 @@ app.use(cors({
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
 app.use('/doctors', doctorRoutes);
 app.use('/appointments', appointmentRoutes);
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI,{dbname:'AppointmentBooking'})
-  .then(() => app.listen(process.env.port, () => console.log('Server running on port 5000')))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, { dbname: 'AppointmentBooking' })
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));  
+  })
+  .catch(err => console.error(err));
